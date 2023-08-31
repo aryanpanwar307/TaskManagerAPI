cfe880c3cf2a8f43ce96ec0d71d72fb59d26211d
aryanpanwar307 <aryan.panwar.307.ap@gmail.com>
2023-08-31 23:34:23 +0530

    "second commit"

---
+++ MIDDLEWARES/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = require('../MODELS/Users');
+
+const auth = async (req,res) => {
+    try{
+        const token = req.header('Authorization').replace('Bearer ','');
+        const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
+        const user = await User.findOne({
+            _id: decoded._id
+        })
+        if(!user){
+            throw new Error('unable to login, invalid credentials')
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    }
+    catch(error){
+        res.status(401).send(error:error.message)
+    }
+}
